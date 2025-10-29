54c163286a9af1e029882facda48e1d394c33189
okinawa-metro-create <ryukyuresort.airlines@gmail.com>
2025-10-29 19:06:26 +0900

    ハンバーガーメニュー　３

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", () => {
   const hamburger = document.getElementById("hamburger");
-  const navgroup = document.getElementById("navgroup");
+  const navgroup = document.querySelector(".header__navgroup");
 
   hamburger.addEventListener("click", () => {
     navgroup.classList.toggle("show");
